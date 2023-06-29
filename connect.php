4b92fd44243bbebfd75777e1a60687b170d6c10f
Sethchrome45 <109365109+Sethchrome45@users.noreply.github.com>
2023-06-29 10:41:06 -0500

    Update connect.php
    Added credentials

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 $conN = mysqli_init();
 mysqli_ssl_set($conN,NULL,NULL, "C:\Users\sethm\OneDrive\Documents\DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($con, "cmi-server.mysql.database.azure.com", "smensah", "{your_password}", "{your_database}", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($con, "cmi-server.mysql.database.azure.com", "smensah", "Iamsaved2day!", "cmi_db", 3306, MYSQLI_CLIENT_SSL);
 
 ?>
