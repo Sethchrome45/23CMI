ad70c075629793d28560c1cefc796b0089bff2f3
Sethchrome45 <109365109+Sethchrome45@users.noreply.github.com>
2023-06-29 11:58:39 -0500

    Update connect.php
    added link to SSL

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, NULL, NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 mysqli_real_connect($con, "cmi-server.mysql.database.azure.com", "smensah", "Iamsaved2day!", "cmi_db", 3306, MYSQLI_CLIENT_SSL);
 
 ?>
