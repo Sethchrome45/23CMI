d356280ea463ef6250fa90c45b7acdafa904bd29
Sethchrome45 <109365109+Sethchrome45@users.noreply.github.com>
2023-06-29 09:59:33 -0500

    Update connect.php
    $conn to $con

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
 
-mysqli_real_connect($conn, "cmi-server.mysql.database.azure.com", "smensah", "Iamsaved2day!", "cmi_db", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($con, "cmi-server.mysql.database.azure.com", "smensah", "Iamsaved2day!", "cmi_db", 3306, MYSQLI_CLIENT_SSL);
 
 ?>
