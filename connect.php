50a7d192f6a3d46bfbf20857a5a3a1e34f652803
Sethchrome45 <109365109+Sethchrome45@users.noreply.github.com>
2023-06-28 23:11:59 -0500

    Update connect.php
    Updated new database details

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$un="root";
-$pw="";
-$host="localhost";
-$db="cmi";
+$un="smensah";
+$pw="Iamsaved2day!";
+$host="cmi-server.mysql.database.azure.com";
+$db="cmi_db";
 
 $con=mysqli_connect($host,$un,$pw,$db);
 ?>
