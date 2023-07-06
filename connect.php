06cf0026b51ed6000d985fa611cdd39c4bb64f92
Sethchrome45 <109365109+Sethchrome45@users.noreply.github.com>
2023-07-05 22:28:52 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,7 @@
 <?php
-$un="smensah";
-$pw="Iamsaved2day!";
-$host="cmi-server.mysql.database.azure.com";
-$db="cmi_db";
 
-$con=mysqli_connect($host,$un,$pw,$db);
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($con, "cmi-server.mysql.database.azure.com", "smensah", "Iamsaved2day!", "cmi_db", 3306, MYSQLI_CLIENT_SSL);
+
 ?>
